6afa5dc3180b51903ee83e4e0bc335308b33942c
mrL <mrL@mrL-PC>
2015-09-07 14:11:38 +0800

    项目提交

---
+++ src/com/add/Add.java
@@ -0,0 +1,14 @@
+package com.add;
+
+public class Add {
+	public Add() {
+	}
+	
+	public int add(int n1,int n2){
+		int sum;
+		sum=n1+n2;
+		return sum;
+		
+	}
+
+}
---
+++ src/com/add/AddTest.java
@@ -0,0 +1,24 @@
+package com.add;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AddTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testAdd() {
+		fail("尚未实现");
+	}
+
+	@Test
+	public void testAdd1() {
+		fail("尚未实现");
+	}
+
+}
